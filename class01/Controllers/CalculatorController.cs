44180d17cf83292416fca02f53faa8db44b16df9
Renato Lima <renattolima@gmail.com>
2020-08-03 21:33:44 -0300

    Adding calculator controller

---
+++ class01/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace class01.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public CalculatorController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                decimal sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(number, out decimalValue))
+                return decimalValue;
+
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            return double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+        }
+    }
+}
