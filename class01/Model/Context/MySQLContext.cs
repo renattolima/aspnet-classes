978983fb284af9ea0c5df169962f4b6942f3ea0e
Renato Lima <renattolima@gmail.com>
2020-08-14 21:29:05 -0300

    Adding sql connection and database script file

---
+++ class01/Model/Context/MySQLContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace class01.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+        }
+
+        protected MySQLContext()
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
--- class01/Startup.cs
+++ class01/Startup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using class01.Model.Context;
 using class01.Services;
 using class01.Services.Implementations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,8 @@ namespace class01
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
             services.AddControllers();
             services.AddScoped<IPersonService, PersonServiceImpl>();
         }
